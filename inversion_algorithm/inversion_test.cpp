d56dcb8c76e6dd599281ce7a06e65d64252fa07d
Roman Istomin <roman@perm.lan>
2022-02-07 17:48:45 -0500

    added simple backout test

--- inversion_algorithm/inversion_test.cpp
+++ inversion_algorithm/inversion_test.cpp
@@ -30,3 +30,33 @@ TEST(PCMInversion, Basic_calculation) {
         ASSERT_LE(inverted_deltas[i], inverted_deltas[i+1]);
     }
 }
+
+TEST(PCMInversion, Basic_parameter_back_out) {
+    int num_prod = 25;
+    int num_dim = 5;
+    int num_draws = 10000;
+    Eigen::ArrayXd p(num_prod);
+    Eigen::ArrayXd deltas(num_prod);
+
+    Eigen::MatrixXd x = Eigen::MatrixXd::Random(num_prod, num_dim);
+    Eigen::ArrayXd sigma_x = Eigen::ArrayXd::Ones(num_dim);
+    Eigen::ArrayXXd grid = Eigen::ArrayXXd::Random(num_draws, num_dim);
+    Eigen::ArrayXd weights = Eigen::ArrayXd::Ones(num_draws)/num_draws;
+
+    // fill prices and shares
+    for(int i = 1; i<= num_prod; ++i){
+        p[i-1] = pow(i, 1.5);
+        deltas[i-1] = i;
+    }
+
+    auto shares = pcm_share::unc_share(deltas, x, p, 1.0, sigma_x, grid, weights);
+
+    ASSERT_TRUE((shares > 0).all());
+
+    share_inversion::pcm_parameters param(x, p, 1.0, sigma_x, grid, weights, shares);
+
+    auto inverted_deltas = share_inversion::invert_shares(param);
+    
+    ASSERT_LE((inverted_deltas - deltas).abs().sum(), param.delta_step_tolerance * num_prod * 10);
+    
+}
