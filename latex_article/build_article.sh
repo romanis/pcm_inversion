997a61cd8cabd08862bd25cd41e25f5c377d8a91
Roman Istomin <romanistomin@gmail.com>
2025-01-25 20:29:16 -0800

    added removal of temp files

--- latex_article/build_article.sh
+++ latex_article/build_article.sh
@@ -2,3 +2,4 @@ pdflatex PCM\ inversion.tex
 bibtex PCM\ inversion
 pdflatex PCM\ inversion.tex
 pdflatex PCM\ inversion.tex
+rm PCM\ inversion.bbl PCM\ inversion.fls PCM\ inversion.blg PCM\ inversion.log PCM\ inversion.out PCM\ inversion.aux 
